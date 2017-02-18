81bc967941b2cd18bde0d2d3ca73c9b349dadc7d
Patrick <patrickrice20@gmail.com>
2017-02-17 20:17:30 -0800

    add get methods

--- App/CoffeeBrewGuide/app/src/main/java/com/brewguide/android/coffeebrewguide/BrewMethod.java
+++ App/CoffeeBrewGuide/app/src/main/java/com/brewguide/android/coffeebrewguide/BrewMethod.java
@@ -1,11 +1,10 @@
 package com.brewguide.android.coffeebrewguide;
 
+import android.media.Image;
 import javax.xml.datatype.Duration;
 
-import static android.R.attr.name;
-
 /**
- * Created by Patrick on 2/17/2017.
+ * Class holds all information about the brew methods
  */
 
 public class BrewMethod {
@@ -13,13 +12,46 @@ public class BrewMethod {
     String[] methodInstructions;
     int methodServingSize;
     Duration methodBrewTime;
+    Image homeScreenTile, detailActivityGraphic;
 
-
-    public BrewMethod(String name, String[] instructions, int servingSize, Duration brewTime, String grindSize){
+    public BrewMethod(String name, String[] instructions, int servingSize, Duration brewTime, String grindSize, Image tile, Image graphic){
         methodName = name;
         methodGrindSize = grindSize;
         methodInstructions = instructions;
         methodServingSize = servingSize;
         methodBrewTime = brewTime;
+        homeScreenTile = tile;
+        detailActivityGraphic = graphic;
+    }
+    /**
+     * get methods
+     *
+     */
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public String[] getMethodInstructions() {
+        return methodInstructions;
+    }
+
+    public String getMethodGrindSize() {
+        return methodGrindSize;
+    }
+
+    public int getMethodServingSize() {
+        return methodServingSize;
+    }
+
+    public Duration getMethodBrewTime() {
+        return methodBrewTime;
+    }
+
+    public Image getHomeScreenTile() {
+        return homeScreenTile;
+    }
+
+    public Image getDetailActivityGraphic() {
+        return detailActivityGraphic;
     }
 }
