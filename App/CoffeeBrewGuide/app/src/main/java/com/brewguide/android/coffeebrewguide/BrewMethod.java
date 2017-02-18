ef25e007b548d374cbad4d324aa03ef4652ca4ca
Patrick <patrickrice20@gmail.com>
2017-02-17 20:09:28 -0800

    actually add the class

--- App/CoffeeBrewGuide/app/src/main/java/com/brewguide/android/coffeebrewguide/BrewMethod.java
+++ App/CoffeeBrewGuide/app/src/main/java/com/brewguide/android/coffeebrewguide/BrewMethod.java
@@ -1,8 +1,25 @@
 package com.brewguide.android.coffeebrewguide;
 
+import javax.xml.datatype.Duration;
+
+import static android.R.attr.name;
+
 /**
  * Created by Patrick on 2/17/2017.
  */
 
 public class BrewMethod {
+    public String methodName, methodGrindSize;
+    String[] methodInstructions;
+    int methodServingSize;
+    Duration methodBrewTime;
+
+
+    public BrewMethod(String name, String[] instructions, int servingSize, Duration brewTime, String grindSize){
+        methodName = name;
+        methodGrindSize = grindSize;
+        methodInstructions = instructions;
+        methodServingSize = servingSize;
+        methodBrewTime = brewTime;
+    }
 }
