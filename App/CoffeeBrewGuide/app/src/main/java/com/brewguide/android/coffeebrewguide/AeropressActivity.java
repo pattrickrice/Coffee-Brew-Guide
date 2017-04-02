44f3bdda2cf9e7c163f3718a6cea5b82a4f4db1a
Patrick <patrickrice20@gmail.com>
2017-04-01 20:51:39 -0700

    change FAB icon to clock, add iconset, dynamically add brewtime, grindsize, servingsize

--- App/CoffeeBrewGuide/app/src/main/java/com/brewguide/android/coffeebrewguide/AeropressActivity.java
+++ App/CoffeeBrewGuide/app/src/main/java/com/brewguide/android/coffeebrewguide/AeropressActivity.java
@@ -3,6 +3,7 @@ package com.brewguide.android.coffeebrewguide;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -12,6 +13,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.joda.time.Duration;
+import org.joda.time.Period;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.PeriodFormatter;
+import org.joda.time.format.PeriodFormatterBuilder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -65,12 +73,40 @@ public class AeropressActivity extends AppCompatActivity  {
         TextView servingNumberTV = (TextView) findViewById(R.id.TV_servingNumber);
         servingNumberTV.setText(serving);
 
+        //set serving Dose
+        //TODO dynamically change units
+        String servingDose = Integer.toString(aeropress.getmMethodServingSize()) + "g";
+        TextView servingSizeTV = (TextView) findViewById(R.id.TV_servingDose);
+        servingSizeTV.setText(servingDose);
+
+        //set brewtime using Joda Time
+        Duration brewTimeJoda = aeropress.getmMethodBrewTime();
+
+        Period period = brewTimeJoda.toPeriod();
+        // format brewtime
+        PeriodFormatter minutesAndSeconds = new PeriodFormatterBuilder()
+                .printZeroAlways()
+                .appendMinutes()
+                .appendSeparator(":")
+                .appendSeconds()
+                .toFormatter();
+        String result = minutesAndSeconds.print(period);
+        TextView brewTimeTV = (TextView) findViewById(R.id.TV_timer);
+        brewTimeTV.setText(result);
+
+        //set grind setting
+        String grindSetting = aeropress.getmMethodGrindSize();
+        TextView grindSettingTV = (TextView) findViewById(R.id.TV_grindSetting);
+        grindSettingTV.setText(grindSetting);
+
         // Auto generated
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_alarm_white_48dp));
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -89,6 +125,7 @@ public class AeropressActivity extends AppCompatActivity  {
         instructions = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.instructions_aeropress_array)));
         servingNumber = 1;
         servingSize = 16;
+        grindSize = getResources().getString(R.string.grind_size_medium);
 
         // Set brewtime to 1:30 (mm:ss)
         brewTime = org.joda.time.Duration.millis(90000);
